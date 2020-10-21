dbd7e3151df21eb68b7bb0a24e723a1b73cea71a
MichaelHempenstall <mhempenstall@gmail.com>
2020-10-21 20:36:54 +0100

    Test Update

--- DairyDashboard/Startup.cs
+++ DairyDashboard/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace DairyDashboard
 {
+    //test comment for Git 
     public class Startup
     {
         public Startup(IConfiguration configuration)
